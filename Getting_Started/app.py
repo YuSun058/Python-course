ae97509bc4b5ab08b3c7105d7a5dbe6f01e8e332
YuSun058 <664207017@qq.com>
2021-08-15 21:43:05 +0800

    update

---
+++ Getting_Started/app.py
@@ -0,0 +1,18 @@
+"""
+Ctrl + ` to call the terminal
+"""
+print("Hello World! ")
+print('*' * 10)
+
+"""
+Use Pylint to look for errors: Ctrl + Shift + M
+"""
+# print "Hello World!"
+
+# 2 +
+
+
+"""
+Format the code: Ctrl+Shift+P and search Format Doc
+"""
+# x=1
